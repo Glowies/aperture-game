5158b4a3e08db97da783b2762c6da0a2fd37b02d
glowies <comuokta@gmail.com>
2023-02-16 16:55:22 -0500

    feat: highlight interactables

--- Assets/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -6,11 +6,46 @@ public class Interactor : MonoBehaviour
 {
     private CharacterController _controller;
     private List<Interactable> _closeInteractables;
+    private Interactable _targetInteractable;
 
     void Awake()
     {
         TryGetComponent(out _controller);
         _closeInteractables = new();
+    }
+
+    void Update()
+    {
+        FindClosestInteractable();
+    }
+
+    private void FindClosestInteractable()
+    {
+        if(_closeInteractables.Count == 0)
+        {
+            return;
+        }
+
+        var prevTarget = _targetInteractable;
+
+        var minDistance = float.PositiveInfinity;
+        foreach(var interactable in _closeInteractables)
+        {
+            var position = interactable.transform.position;
+            var currDistance = Vector3.Distance(transform.position, position);
+            if(currDistance < minDistance)
+            {
+                minDistance = currDistance;
+                _targetInteractable = interactable;
+            }
+        }
+
+        // Update highlights if new target
+        if(prevTarget != _targetInteractable)
+        {
+            prevTarget?.ToggleHighlight(false);
+            _targetInteractable.ToggleHighlight(true);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,14 +67,18 @@ public class Interactor : MonoBehaviour
             return;
         }
         
+        interactable.ToggleHighlight(false);
+        _targetInteractable = null;
         _closeInteractables.Remove(interactable);
     }
 
     public void OnInteract()
     {
-        foreach(var interactable in _closeInteractables)
+        if(_targetInteractable == null)
         {
-            interactable.Interact(_controller);
+            return;
         }
+
+        _targetInteractable.Interact(_controller);
     }
 }
