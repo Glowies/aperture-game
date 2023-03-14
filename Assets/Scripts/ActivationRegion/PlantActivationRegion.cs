9797d5528fad1bd74807d786f5e737e27b2e453a
ed29leung <81656509+ed29leung@users.noreply.github.com>
2023-03-14 17:44:17 -0400

    Enforce only one plant in activation region

--- Assets/Scripts/ActivationRegion/PlantActivationRegion.cs
+++ Assets/Scripts/ActivationRegion/PlantActivationRegion.cs
@@ -8,6 +8,12 @@ public class PlantActivationRegion : MonoBehaviour
     public UnityEvent OnEnter;
     public UnityEvent OnExit;
 
+    //boolean to check if the region already has a plant
+    public bool hasPlant; 
+
+    // the current plant object in the activation region
+    public Plant currentPlant;
+
     private void OnTriggerEnter(Collider other)
     {
         // Only register entry if it's a plant
@@ -15,7 +21,14 @@ public class PlantActivationRegion : MonoBehaviour
         {
             return;
         }
-        
+        // if plant already in this region do nothing
+        if (hasPlant){
+            return;
+        }
+        //set hasPlant to true 
+        hasPlant = true;
+        // set the currentPlant plant object
+        currentPlant = plant;
         plant.ToggleDeadPlant(false);
         plant.TogglePastPlantMesh(false);
 
@@ -29,10 +42,17 @@ public class PlantActivationRegion : MonoBehaviour
         {
             return;
         }
-
+        //check if the plant exiting region is the activation region's current plant
+        if (plant != currentPlant){
+            // if so, don't do anything
+            return;
+        }
+        
         plant.ToggleDeadPlant(true);
         plant.TogglePastPlantMesh(true);
 
+        hasPlant = false;
+        currentPlant = null;
         OnExit.Invoke();
     }
 }
