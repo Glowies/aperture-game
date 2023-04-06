e8951ec8e65c51e3dfe5246e11ae590c76f13742
ed29leung <81656509+ed29leung@users.noreply.github.com>
2023-04-06 16:06:37 -0400

    Removed Teleport object to drop point
    This drop point ignores walls so I removed it to prevent objects getting stuck.

--- Assets/Scripts/Thrower.cs
+++ Assets/Scripts/Thrower.cs
@@ -21,8 +21,7 @@ public class Thrower : MonoBehaviour
 
     //Location where picked up object is held 
     [SerializeField] Transform pickupTarget;
-
-    [SerializeField] Transform dropTarget;
+    
     //tuning the object carry physics 
     private float objectSpeed = 12f;
     //maximum distance the Throwable can be away from the player
@@ -108,7 +107,6 @@ public class Thrower : MonoBehaviour
         
         IsGrabbing = false;
         GrabbedThrowable.Throw(throwDireciton * ThrowForce);
-        GrabbedThrowable.transform.position = dropTarget.position;
         GrabbedThrowable = null;
         grabbedRigidBody = null;
     }
